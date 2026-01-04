1df79060a19ec24ca8dcf002a640c01942588c27
cheshanth-14 <cheshanth14@gmail.com>
2026-01-04 21:16:08 +0530

    doc updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/estate-agent-app/',
+  build: {
+    outDir: 'docs', // output directly to docs
+  },
 })
